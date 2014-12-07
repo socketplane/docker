5b37b85b14db53c6c366972448a660fa6a51f50c
Madhu Venugopal <madhu@socketplane.io>
2014-12-07 04:46:20 -0800

    Upgrading libcontainer to v0.1.1
    Due to the crazy vendoring process, I made a mistake by pushing the
    libcontainer specific changes into docker directly. Hence I reverted
    that change using ca64798526fd110950405b26132acc07baf5968f.
    
    This commit fixes the revert by updating the libcontainer to v0.1.1
    which carries the fix that is reverted.
    
    Signed-off-by: Madhu Venugopal <madhu@socketplane.io>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -71,7 +71,7 @@ if [ "$1" = '--go' ]; then
 	mv tmp-tar src/code.google.com/p/go/src/pkg/archive/tar
 fi
 
-clone git github.com/socketplane/libcontainer v0.1 github.com/docker/libcontainer
+clone git github.com/socketplane/libcontainer v0.1.1 github.com/docker/libcontainer
 # see src/github.com/docker/libcontainer/update-vendor.sh which is the "source of truth" for libcontainer deps (just like this file)
 rm -rf src/github.com/docker/libcontainer/vendor
 eval "$(grep '^clone ' src/github.com/docker/libcontainer/update-vendor.sh | grep -v 'github.com/codegangsta/cli')"
